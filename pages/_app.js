5d5719441b661b9a934075fd6ff70ebf7b741b99
SmitBhamwala <smitbhamwala@gmail.com>
2024-03-26 17:49:08 +0530

    Added Analytics and Speed Insights for Vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout/layout";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 //To not get Type ERROR "self-signed certificate in certificate chain" while fetching data from Firebase
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
@@ -11,6 +13,8 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
     </Layout>
   );
 }
